361e9e8cd4247e0945b26748bdbcddea0117484d
Devid <devidtroka@gmail.com>
2025-05-26 20:49:50 +0200

    Aggiunto routing alla pagina eventi
    Aggiunto il routing alla pagina del singolo evento dall'index. Pagina correttamente caricata dal db.

--- src/service/routes/main.js
+++ src/service/routes/main.js
@@ -32,6 +32,23 @@ function insertEventData(){
 */
 router.get('/about', (req, res) =>{
     res.send('about');
-})
+});
+
+//EVENT PAGE ROUTING
+router.get('/event/:id', async (req, res) => {
+    try{
+        let slug = req.params.id;
+        const data = await Event.findById({_id: slug});
+        const locals = {
+            title : data.title,
+            description : data.description,
+            showLayoutParts: true
+        }
+        res.render('event',{locals, data});
+    }catch (e) {
+        //error page
+    }
+    //res.send('index');
+});
 
 module.exports = router;

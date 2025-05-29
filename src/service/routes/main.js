4193f72e3efe047ec872a75f64fe1a4905fc3866
Devid <devidtroka@gmail.com>
2025-05-29 22:01:14 +0200

    Aggiunto sistema di ricerca eventi
    Implementata la funzionalità di ricerca eventi; per titolo o descrizione, con indirizzamento alla pagina risultati.

--- src/service/routes/main.js
+++ src/service/routes/main.js
@@ -82,4 +82,29 @@ router.get('/event/:id', async (req, res) => {
     //res.send('index');
 });
 
+//POST ROUTE
+//POST search
+router.post('/search', async (req, res) => {
+    try{
+        const locals = {
+            title : "intn",
+            description : "events webapp",
+            showLayoutParts: true
+        }
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-O]/g, ""); //RIMOZIONE CARATTERI SPECIALI
+
+        const data = await Event.find({
+            $or: [
+                {title: {$regex: new RegExp(searchNoSpecialChar, 'i')}},
+                {description: {$regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        });
+        res.render('search', { locals, data });
+    }catch (e) {
+        //error page
+    }
+    //res.send('index');
+});
+
 module.exports = router;

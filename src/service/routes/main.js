807eaf9bdbe6d88cd97ce109099293f577ae7175
Devid <devidtroka@gmail.com>
2025-06-03 21:04:29 +0200

    Aggiunta eventi Open Data Trento
    Aggiunti gli eventi del comune di Trento alla homepage, mediante Open Data del comune.

--- src/service/routes/main.js
+++ src/service/routes/main.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/Event');
+const User = require('../models/User');
+const axios = require('axios');
+
 //ROUTING CON INDICATORE NUMERO DELLE PAGINE
 router.get('/', async (req, res) => {
     try{
@@ -20,9 +23,12 @@ router.get('/', async (req, res) => {
         const count = await Event.countDocuments();
         const nextPage = page + 1;
         const hasNextPage = nextPage < Math.ceil(count / perPage);
+        const trentoApi = await axios.get('https://www.comune.trento.it/api/opendata/v2/content/search/classes%20%27event%27');
+        const dataComune = trentoApi.data.searchHits;
 
         res.render('index', { locals,
             data,
+            dataComune,
             current: page,
             nextPage: hasNextPage ? nextPage : null
         });
@@ -65,23 +71,49 @@ router.get('/about', (req, res) =>{
     res.send('about');
 });
 
-//EVENT PAGE ROUTING
+//EVENT PAGE ROUTING DA RENDERE PIù OTTIMIZZATA!!!!
 router.get('/event/:id', async (req, res) => {
     try{
-        let slug = req.params.id;
-        const data = await Event.findById({_id: slug});
-        const options = { day: '2-digit', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' };
-        const formattedDate = new Date(data.date).toLocaleString('it-IT', options);
-        const locals = {
-            title : data.title,
-            description : data.description,
-            location: data.location,
-            date: formattedDate,
-            tag : data.tag,
-            image : data.imageUrl,
-            showLayoutParts: true
+        const slug = req.params.id;
+        if(slug.startWith("trento-")){
+            const trentoId = slug.split("trento-")[1];
+            const response = await axios.get(`https://www.comune.trento.it/api/opendata/v2/content/read/${trentoId}`);
+
+            const event = response.data;
+            const title = event.metadata.name["ita-IT"] || "Evento Comune di Trento";
+            const description = event.metadata.description?.["ita-IT"] || "Nessuna descrizione disponibile";
+            const location = event.metadata.address?.["ita-IT"] || "Trento";
+            const date = event.metadata.published;
+            const formattedDate = new Date(date).toLocaleString('it-IT', {
+                day: '2-digit', month: '2-digit', year: 'numeric',
+                hour: '2-digit', minute: '2-digit'
+            });
+            const image = event.metadata.image_url || "";
+            const locals = {
+                title,
+                description,
+                location,
+                date: formattedDate,
+                tag: "Comune di Trento",
+                image,
+                showLayoutParts: true
+            };
+            return res.render('event', { locals, data: null, mapboxToken: process.env.MAPBOX_TOKEN });
+        }else {
+            const data = await Event.findById(slug);
+            const options = {day: '2-digit', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit'};
+            const formattedDate = new Date(data.date).toLocaleString('it-IT', options);
+            const locals = {
+                title: data.title,
+                description: data.description,
+                location: data.location,
+                date: formattedDate,
+                tag: data.tag,
+                image: data.imageUrl,
+                showLayoutParts: true
+            };
+            res.render('event', {locals, data, mapboxToken: process.env.MAPBOX_TOKEN});
         }
-        res.render('event',{locals, data, mapboxToken: process.env.MAPBOX_TOKEN });
     }catch (e) {
         //error page
     }
